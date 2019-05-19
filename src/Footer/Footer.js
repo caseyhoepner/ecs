b06d1b4ceed02f89e5f8522a97b7e9fa8427a349
Casey Hoepner <caseyhoepner@gmail.com>
2019-05-19 13:55:44 -0500

    Add content to footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,8 +3,20 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className='s-container'>
-      <h1>Footer</h1>
+    <div className='f-container'>
+      <div>
+        <h2>Contact</h2>
+          <p>david.hoepner@att.net</p>
+          <p>(972) 672-4311</p>
+          <h3>Hours</h3>
+            <p>Monday: 9:00 - 5:00</p>
+            <p>Tueday: 9:00 - 5:00</p>
+            <p>Wednesday: 9:00 - 5:00</p>
+            <p>Thursday: 9:00 - 5:00</p>
+            <p>Friday: 9:00 - 5:00</p>
+            <p>Saturday: Closed</p>
+            <p>Sunday: Closed</p>
+        </div>
     </div>
   )
 }
