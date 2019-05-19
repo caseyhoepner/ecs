5fd43b67f11b0ec586b5c17cf2264b54de586093
Casey Hoepner <caseyhoepner@gmail.com>
2019-05-18 21:17:31 -0500

    Set up browser router and switch routes for home and about

--- src/App/index.js
+++ src/App/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Route, withRouter, Switch } from 'react-router-dom';
+
 import SocialBar from '../SocialBar/SocialBar.js';
 import NavBar from '../NavBar/NavBar.js';
 import Home from '../Home/Home.js';
 import About from '../About/About.js';
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <SocialBar />
-      <NavBar />
-      <Home />
-      <About />
+      <Route path='/' component={SocialBar} />
+      <Route path='/' component={NavBar} />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
