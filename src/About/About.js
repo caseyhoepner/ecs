3fa8f12c588777dec0376bfe698467de2fbdab87
Casey Hoepner <caseyhoepner@gmail.com>
2019-05-19 17:49:13 -0500

    Add content to About page

--- src/About/About.js
+++ src/About/About.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './About.css';
 
-export class About extends Component {
-  render() {
-    return (
-      <div>
-        <p>About page</p>
-      </div>
-    )
-  }
+const About = () => {
+  return (
+    <div className='a-container'>
+      <h1>About Us</h1>
+      <p className='a-text'>Engineered Construction Solutions is a construction and remodeling company serving the North Dallas area. It was formed in July of 2010 when Dave Hoepner, owner and operator, turned his lifelong hobby of construction and design into a full-time endeavor.</p>
+      <p className='a-text'>Dave's experience covers ground-up construction as well as all phases of remodeling. His mechanical mind enables him to troubleshoot issues big and small and his creative design techniques allow him to think outside the box and offer suggestions to customers that they may not have considered.</p>
+      <p className='a-text'>Dave's pride in his work drives his desire to produce high quality results for his clients.</p>
+    </div>
+  )
 }
 
 export default About;
