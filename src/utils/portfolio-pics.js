1f05575ea12396e88fb1de1780f8ebc5d404f1ce
Casey Hoepner <caseyhoepner@gmail.com>
2019-08-23 15:05:28 +0100

    Build out portfolio dataset

---
+++ src/utils/portfolio-pics.js
@@ -0,0 +1,141 @@
+export const portfolioPics = [
+  {
+    category: 'Bathrooms',
+    mainPic: 'bathrooms/bathroom1/1.jpg',
+    bathrooms: [
+      {
+        decription: 'bathroom 1 description',
+        pics: [
+          {
+            pic: 'bathrooms/bathroom1/1.jpg',
+            description: 'pic1',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom1/2.jpg',
+            description: 'pic2',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom1/3.jpg',
+            description: 'pic3',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom1/4.jpg',
+            description: 'pic4',
+            before: false,
+          },
+        ]
+      },
+      {
+        decription: 'bathroom 2 description',
+        pics: [
+          {
+            pic: 'bathrooms/bathroom2/1.jpg',
+            description: 'pic1',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom2/2.jpg',
+            description: 'pic2',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom2/3.jpg',
+            description: 'pic3',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom2/4.jpg',
+            description: 'pic4',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom2/5.jpg',
+            description: 'pic5',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom2/6.jpg',
+            description: 'pic6',
+            before: false,
+          },
+        ]
+      },
+      {
+        decription: 'bathroom 3 description',
+        pics: [
+          {
+            pic: 'bathrooms/bathroom3/1.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/2.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/3.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/4.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/5.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/6.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/7.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/8.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/9.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/10.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/11.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/12.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/13.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/14.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/15.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/16.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/17.jpg',
+            description: '',
+            before: false,
+          }, {
+            pic: 'bathrooms/bathroom3/18.jpg',
+            description: '',
+            before: false,
+          },
+        ]
+      },
+
+
+    ],
+  },
+];
+
